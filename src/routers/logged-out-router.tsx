868dd770c9cafbe96a51e0d15bd9c43a17cbdeaf
eungwang <scm323@naver.com>
2022-10-11 00:26:10 +0900

    styled

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NotFound } from "../pages/404";
 import CreateAccount from "../pages/create-account";
 import Login from "../pages/login";
-import { LoggedInRouter } from "./logged-in-router";
 
 export const LoggedOutRouter = () => {
   return (
